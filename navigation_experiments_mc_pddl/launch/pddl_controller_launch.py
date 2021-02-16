dcf65c82e1a7c118dc339327f8436cb99f698294
jginesclavero <jginesclavero@gmail.com>
2021-02-16 13:12:49 +0000

    Change plansys2 launch

--- navigation_experiments_mc_pddl/launch/pddl_controller_launch.py
+++ navigation_experiments_mc_pddl/launch/pddl_controller_launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(
             get_package_share_directory('plansys2_bringup'),
             'launch',
-            'plansys2_bringup_launch_monolithic.py')),
+            'plansys2_bringup_launch_distributed.py')),
         launch_arguments={'model_file': example_dir + '/pddl/patrol_w_recharge.pddl'}.items()
         )
 
